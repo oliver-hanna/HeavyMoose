9773ee31d17f820010e9ffff6fc5d89197220b94
Oliver Hanna <oliver.hanna@colorado.edu>
2015-04-11 21:20:15 -0600

    First word of sentence capitalization implemented.

--- app.py
+++ app.py
@@ -8,9 +8,22 @@ def convert():
     if request.method == 'POST':
         origin = request.form['origin']
         final_string = ""
+        capnext = false
         words_list = origin.split()
         for word in words_list:
-            final_string += thes_search(word) + " "
+            thesWord = thes_search(word)
+            if capnext == true:
+                finalstring += thesWord.title() + " "
+                capnext = false
+            else:
+                final_string += thesWord + " "
+            cl = len(thesWord)-1
+            if
+            (
+                    thesWord[cl] == '!' or thesWord[cl] == '.' or
+                    thesWord[cl] == '?'
+            ):
+                capnext = true
         return render_template('index.html', origin=origin, result=final_string)
     else:
         return render_template('index.html')
